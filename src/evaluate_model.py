4fbfa5787b33e6962a4a57c7828a167fd745c54a
Cuong <41280374+phuccuongngo99@users.noreply.github.com>
2023-11-04 15:09:57 +0800

    Add argparse check

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,6 +1,62 @@
+import os
+import argparse
 import json
+
 import torch
 from transformers import pipeline
+
+def parse_args():
+    def check_valid_path(value):
+        if not os.path.exists(value):
+            raise argparse.ArgumentTypeError(f"The path {value} does not exist")
+        return value
+    
+    def check_squad_dev_path(value):
+        check_valid_path(value)
+        if os.path.basename(value) != 'dev-v1.1.json':
+            raise argparse.ArgumentTypeError("File basename must be 'dev-v1.1.json'")
+        return value
+    
+    def check_json_extension(value):
+        check_valid_path(os.path.dirname(value))
+        base, ext = os.path.splitext(os.path.basename(value))
+        if ext.lower() != '.json':
+            raise argparse.ArgumentTypeError("Output file must have a .json extension")
+        return value
+
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser(description="Run QA model inference.")
+
+    # Add arguments to the parser
+    parser.add_argument(
+        "--dev_json_path",
+        type=check_squad_dev_path,
+        default="../scripts/evaluation/dev-v1.1.json",
+        help="Path to the evaluation dataset JSON file. Default is '../scripts/evaluation/dev-v1.1.json'."
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=128,
+        help="Batch size for processing. Default is 64."
+    )
+    parser.add_argument(
+        "--output_path",
+        type=check_json_extension,
+        required=True,
+        help="Path where predictions should be saved."
+    )
+    parser.add_argument(
+        "--model_path",
+        type=check_valid_path,
+        required=True,
+        help="Path to the local model directory."
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    return args
 
 def load_evaluation_set(dev_json_path):
     question_id = []
@@ -26,20 +82,20 @@ def save_pred_json(question_id, predictions, pred_json_path):
         json.dump(pred_json, f)
 
 
-batch_size = 64
-file_path = "../scripts/evaluation/dev-v1.1.json"
-prediction_path = "../scripts/evaluation/pred.json"
+def evaluate(args):
+    device = 0 if torch.cuda.is_available() else -1
 
-device = 0 if torch.cuda.is_available() else -1
+    question_answerer = pipeline(task="question-answering", device=device, model=args.model_path)
 
-question_answerer = pipeline(task="question-answering", device=device, model="./local_model")
-# question_answerer = pipeline(task="question-answering", device=device)
+    print("Loading evaluation set from {}...".format(args.dev_json_path))
+    question_id, question_context_pair = load_evaluation_set(args.dev_json_path)
 
-print("Loading evaluation set from {}...".format(file_path))
-question_id, question_context_pair = load_evaluation_set(file_path)
+    print("Evaluating with batch size {}...".format(args.batch_size))
+    predictions = question_answerer(question_context_pair, batch_size=args.batch_size)
 
-print("Evaluating with batch size {}...".format(batch_size))
-predictions = question_answerer(question_context_pair, batch_size=batch_size)
+    print("Done evaluating. Saving predictions to {}...".format(args.output_path))
+    save_pred_json(question_id, predictions, args.output_path)
 
-print("Done evaluating. Saving predictions to {}...".format(prediction_path))
-save_pred_json(question_id, predictions, prediction_path)
+if __name__ == "__main__":
+    args = parse_args()
+    evaluate(args)
